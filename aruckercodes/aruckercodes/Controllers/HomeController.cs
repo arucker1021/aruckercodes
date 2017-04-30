d97108153a8f821464e3cabb2a0c81abcfc7d2b3
Antony Rucker <arucker1021@gmail.com>
2017-04-29 22:31:27 -0700

    Content updates

--- aruckercodes/aruckercodes/Controllers/HomeController.cs
+++ aruckercodes/aruckercodes/Controllers/HomeController.cs
@@ -13,18 +13,22 @@ namespace aruckercodes.Controllers
             return View();
         }
 
-        public ActionResult About()
+        public ActionResult WorkHist()
         {
-            ViewBag.Message = "Your application description page.";
-
             return View();
         }
 
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
 
             return View();
         }
+
+        public ActionResult WorkExamples()
+        {
+
+            return View();
+        }
+        
     }
 }
